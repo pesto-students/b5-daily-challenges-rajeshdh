0e021ccfdfe0f2640e7192186a5316a150147ca1
rajesh dhiman <rajesh.dhiman@srijan.net>
2021-01-05 18:41:09 +0530

    Solved math fns

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,14 @@
+// You can use the function from the `Math` module.
+
+const {
+  sqrt,
+  round,
+} = Math;
+const power = (a, b) => a ** b;
+
+// Don't change the exported names.
+export {
+  sqrt,
+  power,
+  round,
+};
